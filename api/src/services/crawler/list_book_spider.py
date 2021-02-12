d0c3779723b42fe8063ab5ed59e6a58bf6175a08
void-kun <hoangzrik@gmail.com>
2021-02-12 12:13:40 +0700

    up

--- api/src/services/crawler/list_book_spider.py
+++ api/src/services/crawler/list_book_spider.py
@@ -7,10 +7,14 @@ class ListBookSpider(scrapy.Spider):
 
     def start_requests(self):
         urls = []
-        for num in range(0, 0):
-            urls.append(f'https://wikidich.com/tim-kiem?status=5794f03dd7ced228f4419192&qs=1&gender=5794f03dd7ced228f4419196&m=2&start={num}&so=4&y=2021&vo=1')   
+        for num in range(0, 1):
+            urls.append(
+                f'https://wikidich.com/tim-kiem?status=5794f03dd7ced228f4419192&qs=1&gender=5794f03dd7ced228f4419196&m=2&start={num}&so=4&y=2021&vo=1')
         for url in urls:
             yield scrapy.Request(url=url, callback=self.parser)
 
     def parser(self, response):
-        print(response.content)
+        for book in response.css('.book-item'):
+            yield {
+                'stats': book.css('.info-col .book-stats-box .book-stats')[0].css('span::text').get()
+            }
