c4a0bd96578c3acc5492e83d0fc7bced7cb460a8
void-kun <hoangzrik@gmail.com>
2021-02-13 15:29:15 +0700

    add cron crawl

--- api/src/services/crawler/list_book_spider.py
+++ api/src/services/crawler/list_book_spider.py
@@ -1,5 +1,5 @@
 import scrapy
-
+from re import search
 
 class ListBookSpider(scrapy.Spider):
     name = 'list_book'
@@ -7,7 +7,7 @@ class ListBookSpider(scrapy.Spider):
 
     def start_requests(self):
         urls = []
-        for num in range(0, 1):
+        for num in range(0, 980, 20):
             urls.append(
                 f'https://wikidich.com/tim-kiem?status=5794f03dd7ced228f4419192&qs=1&gender=5794f03dd7ced228f4419196&m=2&start={num}&so=4&y=2021&vo=1')
         for url in urls:
@@ -15,6 +15,12 @@ class ListBookSpider(scrapy.Spider):
 
     def parser(self, response):
         for book in response.css('.book-item'):
-            yield {
-                'stats': book.css('.info-col .book-stats-box .book-stats')[0].css('span::text').get()
-            }
+            addNum = book.css('.info-col .book-stats-box .book-stats')[0].css('span::text').get()
+            if search(r'[km]', addNum) or search(r'(\d){4,}',addNum):
+                yield {
+                    'title': book.css('.info-col .book-title::text').extract_first(),
+                    'url': book.css('.info-col .tooltipped::attr(href)').extract_first(),
+                    'addNum': addNum,
+                    'author': book.css('.book-author a::text').extract_first(),
+                    'gender': book.css('.book-gender::text').extract_first()
+                }
