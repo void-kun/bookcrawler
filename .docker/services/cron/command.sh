261596f68f00cc27dd0d87ef982a7f80293a74b2
void-kun <hoangzrik@gmail.com>
2021-02-16 07:19:52 +0700

    up

--- .docker/services/cron/command.sh
+++ .docker/services/cron/command.sh
@@ -6,6 +6,7 @@ apt-get upgrade -y
 apt-get install -y gcc cron
 pip install -r requirements.txt
 
+chmod +x /var/www/app/.docker/services/cron/cron.sh
 crontab -l | { cat; echo "*/10 * * * * /var/www/app/.docker/services/cron/cron.sh"; } | crontab -
 
 tail -F /dev/null
