0ea2b495be7055f4e3a06061b5bc8e83f52e0315
void-kun <hoangzrik@gmail.com>
2024-06-13 13:09:00 +0700

    update

---
+++ bws/crates/ui/src/main.rs
@@ -0,0 +1,109 @@
+use eframe::{egui, CreationContext};
+
+#[derive(serde::Deserialize, serde::Serialize)]
+struct MainApp {
+    title: String,
+    value: f32,
+}
+
+impl Default for MainApp {
+    fn default() -> Self {
+        Self {
+            title: "Theeeee".to_string(),
+            value: 0.0,
+        }
+    }
+}
+
+impl MainApp {
+    fn new(cc: &CreationContext<'_>) -> Self {
+        if let Some(storage) = cc.storage {
+            return eframe::get_value(storage, eframe::APP_KEY).unwrap_or_default();
+        }
+        Default::default()
+    }
+}
+
+impl eframe::App for MainApp {
+    fn save(&mut self, storage: &mut dyn eframe::Storage) {
+        eframe::set_value(storage, eframe::APP_KEY, self);
+    }
+
+    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+        egui::TopBottomPanel::top("top_panel").show(ctx, |ui| {
+            egui::menu::bar(ui, |ui| {
+                let is_web = cfg!(target_arch = "wasm32");
+                if !is_web {
+                    ui.menu_button("File", |ui| {
+                        if ui.button("Quit").clicked() {
+                            ctx.send_viewport_cmd(egui::ViewportCommand::Close);
+                        }
+                    });
+                    ui.add_space(16.0);
+                }
+                egui::widgets::global_dark_light_mode_buttons(ui);
+            });
+        });
+
+        egui::CentralPanel::default().show(ctx, |ui| {
+            ui.heading("eframe template");
+
+            ui.horizontal(|ui| {
+                ui.label("Write something: ");
+                ui.text_edit_singleline(&mut self.title);
+            });
+
+            ui.add(egui::Slider::new(&mut self.value, 0.0..=10.0).text("value"));
+            if ui.button("Increment").clicked() {
+                self.value += 1.0;
+            }
+
+            ui.separator();
+            ui.add(egui::github_link_file!(
+                "https://github.com/emilk/eframe_template/blob/main",
+                "Source code"
+            ));
+
+            ui.with_layout(egui::Layout::bottom_up(egui::Align::LEFT), |ui| {
+                powered_by_egui_and_eframe(ui);
+                egui::warn_if_debug_build(ui);
+            });
+        });
+    }
+}
+
+fn powered_by_egui_and_eframe(ui: &mut egui::Ui) {
+    ui.horizontal(|ui| {
+        ui.spacing_mut().item_spacing.x = 0.0;
+        ui.label("Powered by ");
+        ui.hyperlink_to("egui", "https://github.com/emilk/egui");
+        ui.label(" and ");
+        ui.hyperlink_to(
+            "eframe",
+            "https://github.com/emilk/egui/tree/master/crates/eframe",
+        );
+        ui.label(".");
+    });
+}
+
+fn main() -> Result<(), eframe::Error> {
+    env_logger::init();
+    let options = eframe::NativeOptions {
+        viewport: egui::ViewportBuilder::default()
+            .with_inner_size([400.0, 300.0])
+            .with_min_inner_size([300.0, 220.0])
+            .with_icon(
+                eframe::icon_data::from_png_bytes(
+                    &include_bytes!("../../../assets/images/icon-256.png")[..],
+                )
+                .expect("Failed to load icon"),
+            ),
+        ..Default::default()
+    };
+
+    eframe::run_native(
+        "app_name",
+        options,
+        Box::new(|cc| Box::new(MainApp::new(cc))),
+    )
+}
